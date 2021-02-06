d3093de4cea2247939cc90b7ed7673d5fec96f5b
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-02-06 22:12:00 +0100

    continued front end mentor challenge pricing component

---
+++ interactive-pricing-component-main/script.js
@@ -0,0 +1,4 @@
+const numberPageView = document.querySelector('.cart__top--numberPageView');
+const slider = document.querySelector('.cart__slide--slider');
+const amount = document.querySelector('.cart__price--amount');
+const toggle = document.querySelector(".cart__option--toggle");
