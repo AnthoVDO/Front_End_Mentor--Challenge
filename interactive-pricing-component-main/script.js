417f0b5c71b285e3ad5a2727fcd2823dd98d67a9
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-02-07 19:55:14 +0100

    I continued the fron end mentor pricing project. almost finished the javascript and after need to make the responsive

--- interactive-pricing-component-main/script.js
+++ interactive-pricing-component-main/script.js
@@ -2,3 +2,64 @@ const numberPageView = document.querySelector('.cart__top--numberPageView');
 const slider = document.querySelector('.cart__slide--slider');
 const amount = document.querySelector('.cart__price--amount');
 const toggle = document.querySelector(".cart__option--toggle");
+const circleToggle = document.querySelector('.cart__option--circle');
+
+const price = {
+    amout: ["10k", "50k", "100k", "500k", "1M"],
+    perMont: ["8", "12", "16", "24", "36"]
+}
+
+slider.addEventListener("mousedown", (e) => {
+    let value = slider.value;
+    slider.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%)${value}%, hsl(224, 65%, 95%)10%)`
+})
+
+slider.addEventListener("mouseup", (e) => {
+    let value = slider.value;
+    slider.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%)${value}%, hsl(224, 65%, 95%)10%)`
+    let point = 0;
+    if (value < 21) {
+        point = 0;
+    } else {
+        if (value < 41) {
+            point = 1
+        } else {
+            if (value < 61) {
+                point = 2
+            } else {
+                if (value < 81) {
+                    point = 3
+                } else {
+                    if (value < 101) {
+                        point = 4
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+    numberPageView.textContent = price.amout[point];
+    amount.textContent = price.perMont[point];
+})
+
+toggle.addEventListener("click", () => {
+    toggle.classList.toggle('active');
+    if (toggle.classList.contains("active")) {
+        toggle.style.transition = "1s";
+        circleToggle.style.transition = "1s";
+        circleToggle.style.marginLeft = "17px";
+        toggle.style.background = "hsl(174, 86%, 45%)";
+        let beforeReduce = parseInt(amount.value, 10) * 0.75;
+        amount.textContent = beforeReduce.toString();
+    } else {
+        circleToggle.style.transition = "1s";
+        circleToggle.style.marginLeft = "2px";
+        toggle.style.background = "hsl(223, 50%, 87%)";
+        toggle.style.transition = "1s";
+    }
+})
