c5306ee52f3f4942d14ec1acf55b8430cdec26df
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-02-08 22:23:46 +0100

    almost finished pricing component. Javascript done and css done. Need to finish the responsive

--- interactive-pricing-component-main/script.js
+++ interactive-pricing-component-main/script.js
@@ -5,7 +5,7 @@ const toggle = document.querySelector(".cart__option--toggle");
 const circleToggle = document.querySelector('.cart__option--circle');
 
 const price = {
-    amout: ["10k", "50k", "100k", "500k", "1M"],
+    amount: ["10k", "50k", "100k", "500k", "1M"],
     perMont: ["8", "12", "16", "24", "36"]
 }
 
@@ -16,21 +16,26 @@ slider.addEventListener("mousedown", (e) => {
 
 slider.addEventListener("mouseup", (e) => {
     let value = slider.value;
-    slider.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%)${value}%, hsl(224, 65%, 95%)10%)`
+
     let point = 0;
     if (value < 21) {
+        slider.value = 1;
         point = 0;
     } else {
         if (value < 41) {
+            slider.value = 25;
             point = 1
         } else {
             if (value < 61) {
+                slider.value = 50;
                 point = 2
             } else {
                 if (value < 81) {
+                    slider.value = 75;
                     point = 3
                 } else {
                     if (value < 101) {
+                        slider.value = 100;
                         point = 4
                     }
                 }
@@ -40,23 +45,31 @@ slider.addEventListener("mouseup", (e) => {
         }
 
     }
+    slider.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%)${slider.value}%, hsl(224, 65%, 95%)10%)`;
+    let priceValue = price.perMont[point];
+    if (toggle.classList.contains("active")) {
+        priceValue = priceValue * 0.75;
+    } else {
+        priceValue = price.perMont[point];
+    }
 
-
-
-    numberPageView.textContent = price.amout[point];
-    amount.textContent = price.perMont[point];
+    numberPageView.textContent = price.amount[point];
+    amount.textContent = priceValue + ".00";
 })
 
 toggle.addEventListener("click", () => {
+
     toggle.classList.toggle('active');
     if (toggle.classList.contains("active")) {
         toggle.style.transition = "1s";
         circleToggle.style.transition = "1s";
         circleToggle.style.marginLeft = "17px";
         toggle.style.background = "hsl(174, 86%, 45%)";
-        let beforeReduce = parseInt(amount.value, 10) * 0.75;
-        amount.textContent = beforeReduce.toString();
+        let beforeReduce = parseInt(amount.textContent, 10) * 0.75;
+        amount.textContent = beforeReduce.toString() + ".00";
     } else {
+        let afterReduce = parseInt(amount.textContent, 10) / 0.75;
+        amount.textContent = afterReduce.toString() + ".00";
         circleToggle.style.transition = "1s";
         circleToggle.style.marginLeft = "2px";
         toggle.style.background = "hsl(223, 50%, 87%)";
